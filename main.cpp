2a03e365bd3dc7e400c489bfd09d7ee2d1fd24eb
Syed Ali Haider <s.haiderali2025@gmail.com>
2025-12-04 17:01:10 +0000

    Good

--- main.cpp
+++ main.cpp
@@ -2,60 +2,76 @@
 using namespace std;
 char  Number[3][3]={ {'1','2','3',}, {'4','5','6'},{'7','8','9'} };
 char Tern='x';
-int userchoice;
-int boder()
+int userchoice,row,colum;
+void  boder()
 {
+    system("cls");
     cout<<endl;
+
     cout<<"\t"<<Number[0][0]<<"\t" <<"|"<<"\t"    <<Number[0][1]<<"\t"    <<"|"<<"\t"      <<Number[0][2]<<"\t"<<" "<<endl;
     cout<<"-------------------------------------------------------------------------------------------\n";
     cout<<"\t"<<Number[1][0]<<"\t" <<"|"<<"\t"     <<Number[1][1]<<"\t"   <<"|"<<"\t"      <<Number[1][2]<<"\t"<<" "<<endl;
     cout<<"----------------------------------------------------------------------------------------------"<<endl;
     cout<<"\t"<<Number[2][0] <<"\t"<<"|"<<"\t"     <<Number[2][1]<<"\t"   <<"|"<<"\t"      <<Number[2][2]<<"\t"<<" "<<endl;
-    cout<<"-------------------------------------------------------------------------------------------------"<<endl;
-    return 0;
+    cout<<"---------------------------|--------------------------------------|-----------------------------"<<endl;
 
 }
-int main()
+void Player_tern()
 {
-cout<<"Welcome ALI TIC -TAC -TOE GAME ";
-boder();
-if(Tern=='x')
-{
-    cout<<"player1[x] Tern:"<<endl;
-    cin>>userchoice;
-    Tern='o';
+    if(Tern=='x')
+    cout<<"Player1[x] Tern";
+else if(Tern=='o')
+cout<<"Player2[O] Tern";
+    cout<<"Enter your choice\n";
+cin>>userchoice;
     switch(userchoice)
     {
-        case 1: Number[0][0] = 'x';
-        break;  
-       case 2: Number[0][1] = 'x';
-        break; 
-        case 3: Number[0][2] ='x';
+        case 1:row=0,colum=0;
         break;
-        case 4: Number[1][0] ='x';
-        break; 
-        case 5: Number[1][1] = 'x';
-        break; 
-       case 6: Number[1][2] = 'x';
-        break; 
-       case 7: Number[2][0] = 'x';
-        break; 
-      case 8: Number[2][1] = 'x';
+        case 2:row=0,colum=1;
         break;
-     
-    case 9: Number[2][2] = 'x';
+        case 3:row=0,colum=2;
         break;
-    default:
-    cout<<"Invalid choice" ;}
-}
-else if(Tern=='0')
+        case 4:row=1,colum=0;
+        break;
+        case 5:row=1,colum=1;
+        break;
+        case 6:row=1,colum=2;
+        break;
+        case 7:row=2,colum=0;
+        break;
+        case 8:row=2,colum=1;
+        break;
+        case 9:row=2,colum=2;
+        break;
+        default:
+        cout<<"Invalid choice ";
+    }
+    if(Tern=='x')
+    {
+        Number[row][colum]='x';
+        Tern='o';
+        }  
+        else if (Tern=='o')
+        {
+            Number[row][colum]='o';
+            Tern='x';
+        }
+        else 
+        cout<<"Invalid Choice";
+    }
+
+int main()
 {
-    cout<<"Player2[O] Tern:"<<endl;
-    cin>>userchoice;
-    Tern='x';
-}
-else 
-cout<<"INvalid choice Try again!";
+cout<<"\tWelcome ALI TIC -TAC -TOE GAME \n";
+while(true)
+{
+
+
+boder();
+Player_tern();
 boder();
 return 0;
+}
+
 }
